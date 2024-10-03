353e3a574e997d2f70d5c8ffdce5ab7a303f938f
Mikel <mgoyago23wg@ikzubirimanteo.com>
2024-10-03 10:10:05 +0200

    Joku infinitua

--- js/script.js
+++ js/script.js
@@ -1,37 +1,24 @@
-const startButton = document.getElementById('hasiera-botoia');
 const targetArea = document.getElementById('jokuaren_eremua');
-const scoreBoard = document.getElementById('puntuazioa');
-const timeLeftBoard = document.getElementById('denbora');
-const pointsBoard = document.getElementById('puntuak');
+const scoreBoard = document.getElementById('puntuak');
 
 let puntuazioa = 0;
-let timeLeft = 30;
 let gameInterval;
-let speedFactor = 1;
+let speedFactor = 0.5;
 let totalPoints = 0;
 let biderkatzailea=1;
 
+startGame();
+
 function startGame() {
     puntuazioa = 0;
-    timeLeft = 30;
     scoreBoard.textContent = puntuazioa;
-    timeLeftBoard.textContent = timeLeft;
-
-    document.getElementById('hasiera-menua').style.display = 'none';
     targetArea.innerHTML = ''; 
 
     gameInterval = setInterval(() => {
-        if (timeLeft > 0) {
-            timeLeft--;
-            timeLeftBoard.textContent = timeLeft;
-            createTarget();
-        } else {
-            clearInterval(gameInterval);
-            alert('Denbora amaitu da! Zure puntuazioa: ' + puntuazioa);
-            totalPoints += puntuazioa;
-            pointsBoard.textContent = totalPoints;
-            document.getElementById('hasiera-menua').style.display = 'block';
-        }
+            let random=Math.floor(Math.random() * 3)+1;
+            for (let i=0; i<random;i++){
+                createTarget();
+            }
     }, 1000 / speedFactor);
 }
 
@@ -57,35 +44,3 @@ function createTarget() {
         diana.remove();
     });
 }
-
-/*targetArea.addEventListener('click', (event) => {
-    shootAnimation(event.clientX, event.clientY); // Dispara desde la posición del clic
-});
-
-function shootAnimation(mouseX, mouseY) {
-    const bullet = document.createElement('div');
-    bullet.classList.add('bullet');
-    bullet.style.left = `${mouseX}px`;
-    bullet.style.top = `${mouseY}px`;
-    document.body.appendChild(bullet);
-
-    // Calcular la posición del objetivo para animar la bala hacia él
-    const targetX = mouseX + 10; // Ajuste para que la bala apunte hacia el objetivo
-    const targetY = mouseY - 50; // Ajuste para que la bala suba un poco al disparar
-
-    // Animación de la bala
-    bullet.animate([
-        { transform: 'translate(0, 0)' },
-        { transform: `translate(${targetX - mouseX}px, ${targetY - mouseY}px)` }
-    ], {
-        duration: 300,
-        fill: 'forwards'
-    });
-
-    // Eliminar la bala después de la animación
-    setTimeout(() => {
-        bullet.remove();
-    }, 300); // Duración de la animación
-}*/
-
-startButton.addEventListener('click', startGame);
